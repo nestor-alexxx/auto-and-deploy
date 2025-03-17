4a161b2c955d22345f69bc27b05901fa3d7a6334
NESTOR <nestor.alexxx.izh@gmail.com>
2025-03-17 21:00:26 +0600

    убрал коммент

--- generate-sales-data.py
+++ generate-sales-data.py
@@ -11,12 +11,12 @@ COMPANIES = eval(config['Companies']['COMPANIES'])
 today = datetime.today()
 yesterday = today -  timedelta(days=1)
 
-# if 1 <= today.weekday() <= 5:
-d = {
-    'dt': [yesterday.strftime('%Y-%m-%d')] * len(COMPANIES) * 2, 
-    'company': COMPANIES * 2,
-    'transaction_type': ['buy'] * len(COMPANIES) + ['sell'] * len(COMPANIES),
-    'amount': [randint(0, 1000) for _ in range(len(COMPANIES)*2)]
-}
-df = pd.DataFrame(d)
-df.to_csv('sales_data.csv', index=False)
+if 1 <= today.weekday() <= 5:
+    d = {
+        'dt': [yesterday.strftime('%Y-%m-%d')] * len(COMPANIES) * 2, 
+        'company': COMPANIES * 2,
+        'transaction_type': ['buy'] * len(COMPANIES) + ['sell'] * len(COMPANIES),
+        'amount': [randint(0, 1000) for _ in range(len(COMPANIES)*2)]
+    }
+    df = pd.DataFrame(d)
+    df.to_csv('sales_data.csv', index=False)
